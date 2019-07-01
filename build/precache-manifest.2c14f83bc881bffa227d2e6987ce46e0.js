a8f521150d6f8707362067b48c1a0dd730bad8c3
Mary Anthony <mary@blockstack.com>
2019-07-01 13:57:25 -0700

    updating removing broken web Updating animals Updating all the files
    Signed-off-by: Mary Anthony <mary@blockstack.com>

---
+++ build/precache-manifest.2c14f83bc881bffa227d2e6987ce46e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b58b9f576f4af29354520b50322f377",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98a565b1423cdf5c1d7e",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "719bda99885de8407a94",
+    "url": "/static/css/main.20034467.chunk.css"
+  },
+  {
+    "revision": "98a565b1423cdf5c1d7e",
+    "url": "/static/js/2.a2afada9.chunk.js"
+  },
+  {
+    "revision": "719bda99885de8407a94",
+    "url": "/static/js/main.2e9e3d0c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
