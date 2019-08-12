d47c2e29989bfe713e62feb2f8ed11b1af1564c6
Mary Anthony <mary@blockstack.com>
2019-08-11 19:31:38 -0700

    Close #332, #333,#334,#335,#336,#337,#338,#339,#340,#341,#342,#343
    Signed-off-by: Mary Anthony <mary@blockstack.com>

--- build/precache-manifest.d0f786b27b30d35e5ed3a315ee73d206.js
+++ build/precache-manifest.d0f786b27b30d35e5ed3a315ee73d206.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0506b5a7e4f7d43c5e3e4c56e795aaf1",
+    "revision": "e4be23c150b9affc5dab29b4971e438b",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "35a39d29105a866f96b1",
+    "revision": "2c475d0d8982f72da20b",
     "url": "/static/css/main.20034467.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.706862d4.chunk.js"
   },
   {
-    "revision": "35a39d29105a866f96b1",
-    "url": "/static/js/main.5d99a1c1.chunk.js"
+    "revision": "2c475d0d8982f72da20b",
+    "url": "/static/js/main.e3ba0e9e.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
