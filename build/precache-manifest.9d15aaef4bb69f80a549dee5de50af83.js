0a2e1a41013e68485d25fac0a8f89c68aa5a2b7b
Mary Anthony <mary@blockstack.com>
2019-11-16 16:29:30 -0800

    updating with build
    Signed-off-by: Mary Anthony <mary@blockstack.com>

---
+++ build/precache-manifest.9d15aaef4bb69f80a549dee5de50af83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff07b965ff7521affade30a5319a687",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98a565b1423cdf5c1d7e",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "775ff3e53178ac2c218c",
+    "url": "/static/css/main.20034467.chunk.css"
+  },
+  {
+    "revision": "98a565b1423cdf5c1d7e",
+    "url": "/static/js/2.a2afada9.chunk.js"
+  },
+  {
+    "revision": "775ff3e53178ac2c218c",
+    "url": "/static/js/main.2cd60792.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
