a7b37ac41ba10383823b227da27c228c73754bfd
Mary Anthony <mary@blockstack.com>
2019-11-16 16:23:19 -0800

    Fixing the build '
    Signed-off-by: Mary Anthony <mary@blockstack.com>

---
+++ build/precache-manifest.779351a6700b94f586da0706e2a086a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86248e0e6165523e828c3f87d312dfdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98a565b1423cdf5c1d7e",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4ea6b265638896579ed1",
+    "url": "/static/css/main.20034467.chunk.css"
+  },
+  {
+    "revision": "98a565b1423cdf5c1d7e",
+    "url": "/static/js/2.a2afada9.chunk.js"
+  },
+  {
+    "revision": "4ea6b265638896579ed1",
+    "url": "/static/js/main.dc392d67.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
