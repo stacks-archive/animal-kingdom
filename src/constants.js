ff09857c5985e33e5bb36ec774a4e4ab7a6ad7f5
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2019-11-25 17:43:08 +0000

    Zero-to-DApp Tutorial

--- src/constants.js
+++ src/constants.js
@@ -28,6 +28,11 @@ export const TERRITORIES = [
     id: 'tundra',
     name: 'Tundra',
     superpower: 'Let it snow!'
+  },
+  {
+    id: 'westeros',
+    name: 'Westeros',
+    superpower: 'The Iron Throne!'
   }
 ]
 
@@ -52,5 +57,10 @@ export const OTHER_KINGDOMS = [
   {
       app: 'https://thirsty-jang-0c0a17.netlify.com',
       ruler: 'ma1222042.id.blockstack'
+  },
+  {
+    app: 'https://animalkingdoms.netlify.com',
+    ruler: 'meepers.id.blockstack'
   }
+  
 ]
